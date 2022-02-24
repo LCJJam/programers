ac91d58a965bcf4a53ed09c16a8f1f695d5ae270
dlckd <dlckd125@nate.com>
2022-02-25 01:19:29 +0900

    프로그래머스 HASH lv 2

---
+++ ex-Que/src/BetterThanHot.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class BetterThanHot {
+
+    public static void main(String[] args) {
+
+        int answer = 0;
+
+        Integer[] scoville = {1, 2, 3, 9, 10, 12};
+        int K = 7;
+
+        Arrays.sort(scoville);
+
+
+
+        System.out.println(answer);
+    }
+}
