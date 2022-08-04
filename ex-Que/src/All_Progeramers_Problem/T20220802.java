5559310c305eda05952ba34096634c987b4257fb
dlckd <dlckd125@nate.com>
2022-08-05 08:49:12 +0900

    fixed : y set merge -> X Line merge

--- ex-Que/src/All_Progeramers_Problem/T20220802.java
+++ ex-Que/src/All_Progeramers_Problem/T20220802.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class T20220802 {
     public static long solution(int[][] rectangles) {
         long answer = 0;
-
+        long start = System.currentTimeMillis();
         ArrayList<Integer> x_set = new ArrayList<>();
         ArrayList<int[]> y_set = new ArrayList<>();
         for(int[] a : rectangles){
@@ -14,22 +14,42 @@ public class T20220802 {
         }
         Collections.sort(x_set);
         //x_set.sort(Integer::compareTo);
-        Arrays.sort(rectangles,Comparator.comparingInt(o1 -> o1[0]));
-        System.out.println(rectangles[0][0] + " + " + rectangles[0][2]);
-        System.out.println(rectangles[1][0] + " + " + rectangles[1][2]);
-        for(int i = 0; i < x_set.size()-1; i++){
-            y_set.clear();
-            for(int[] b : rectangles){
-                if(x_set.get(i+1) < b[0]) break;
-                if(  b[0] <= x_set.get(i)  &&  x_set.get(i+1) <= b[2]){
-                    y_set.add(new int[]{b[1],b[3]});
+//        Arrays.sort(rectangles,Comparator.comparingInt(o1 -> o1[0]));
+//        for(int i = 0; i < x_set.size()-1; i++){
+//            y_set.clear();
+//            for(int j=0;j<rectangles.length;j++){
+//                int[] b = rectangles[j];
+//                if(x_set.get(i+1) < b[0]) break;
+//                if(  b[0] <= x_set.get(i)  &&  x_set.get(i+1) <= b[2]){
+//                    y_set.add(new int[]{b[1],b[3]});
+//                }
+//            }
+//            int y_val = merge(y_set);
+//            answer += ((long)(x_set.get(i+1)-x_set.get(i)) * (long)y_val);
+//        }
+
+        ArrayList<ArrayList<int[]>> y_2_set = new ArrayList<>();
+        for (int k=0;k<x_set.size();k++){
+            y_2_set.add(k,new ArrayList<int[]>());
+        }
+        for(int j=0;j<rectangles.length;j++){
+            int[] b = rectangles[j];
+            ArrayList<int[]> tmp = new ArrayList<int[]>();
+            for(int i=0;i<x_set.size()-1;i++){
+                if(b[0] <= x_set.get(i)  &&  x_set.get(i+1) <= b[2]){
+                    tmp = new ArrayList<>(y_2_set.get(i));
+                    tmp.add(new int[]{b[1],b[3]});// add(new int[]{b[1],b[3]}
+                    y_2_set.set(i,new ArrayList<>(tmp));
                 }
             }
-            int y_val = merge(y_set);
-            answer += ((long)(x_set.get(i+1)-x_set.get(i)) * (long)y_val);
-            System.out.println(y_set);
+            tmp.clear();
         }
-
+        for(int i=0;i<x_set.size()-1;i++){
+            ArrayList<int[]> a = y_2_set.get(i);
+            int val = merge(a);
+            answer += ((long)(x_set.get(i+1)-x_set.get(i)) * (long)val);
+        }
+        System.out.println(answer);
         return answer;
     }
     public static int merge (ArrayList<int[]> intArr){
